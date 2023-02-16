d30df8c58929e69d96b66ff0c3898ce9aac8e287
Emanuele Simula <simulaemanuele@gmail.com>
2023-02-16 13:12:09 +0100

    feat: implemented function logic given to the PrimaryButton components it can rerun the function it can generate random number within the range and added a feature that show an alert when the user is doing a mistake randomically or intentionally. Check the comments within the code in GameScreen for details.

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Alert} from 'react-native';
 import NumberContainer from '../components/game/NumberContainer';
+import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
 
+//Function it can generate a random number in a certain range excluding one value
 const generateRandomBetween = (
   min: number,
   max: number,
@@ -17,9 +19,45 @@ const generateRandomBetween = (
   }
 };
 
+//variable can dinamically assigned min and max every function call
+let minBoundary = 1;
+let maxBoundary = 100;
+
 const GameScreen = ({userNumber}: {userNumber: number}) => {
-  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  //Initial guess provided by the first call of the random generator function
+  const initialGuess = generateRandomBetween(
+    minBoundary,
+    maxBoundary,
+    userNumber,
+  );
+
+  //the initial guess become the default value of this state
   const [currentGuess, setCurrentGuess] = React.useState(initialGuess);
+
+  //this handler can check the direction of calculation that is "lower" or greater and if the user is doing a mistake return showing an alert
+  const nextGuessHandler = (direction: string) => {
+    if (
+      (direction === 'lower' && currentGuess < userNumber) ||
+      (direction === 'greater' && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie", 'You know that this is wrong...', [
+        {text: 'Sorry!', style: 'cancel'},
+      ]);
+      return;
+    }
+
+    if (direction === 'lower') {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess,
+    );
+    setCurrentGuess(newRndNumber);
+  };
 
   return (
     <View style={styles.screen}>
@@ -27,8 +65,14 @@ const GameScreen = ({userNumber}: {userNumber: number}) => {
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>{'Higher or lower?'}</Text>
-        {/*Button +*/}
-        {/*Button -*/}
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+            -
+          </PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+            +
+          </PrimaryButton>
+        </View>
       </View>
       {/* <View>LOG ROUNDS</View> */}
     </View>
